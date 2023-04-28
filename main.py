dfccb451d516223991b80753de25347252f8f7a4
MattJMaris <mjmaris221@stevenscollege.edu>
2023-04-28 08:03:06 -0400

    Register, Index, and Base html pages added. Need to change 'base.html' to work more for my website theme.

--- main.py
+++ main.py
@@ -12,5 +12,12 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/register', methods=['POST'])
+def create_boats():
+    connection.execute(text("insert into companydb values (:id, :username, :password, :email, :full_name, :type)"), request.form)
+    connection.commit()
+    return render_template('register.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
